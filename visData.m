2b6b84ac81f8cb2a31504a6d48ed3dd814e4089b
Thomas Camminady <thomas.camminady@rwth-aachen.de>
2017-01-05 11:11:23 +0100

    Visualize + Pagerank

--- visData.m
+++ visData.m
@@ -19,12 +19,25 @@ subplot(2,2,4)
 imagesc(Distance);
 title('Distance');
 
+
+
+
+
 N = 50;
+G = digraph( normalize(Traffic(1:N,1:N)));
+
+pr = centrality(G,'pagerank','FollowProbability',0.85);
+G.Nodes.Names = Names(1:N);
+G.Nodes.PageRank = pr;
+G.Nodes.Arriving =sum(Traffic(1:N,1:N))';
+G.Nodes.Departing = sum(Traffic(1:N,1:N),2);
+G.Nodes
+
+
 figure
 geoshow('landareas.shp', 'FaceColor', [0.5 1.0 0.5]);
 hold on
 geoshow(Latitude(1:N),Longitude(1:N),'DisplayType','Point')
-G = digraph( Traffic(1:N,1:N));
 LWidths = G.Edges.Weight/mean(G.Edges.Weight);
 plot(G,'XData',Longitude(1:N),'YData',Latitude(1:N),'LineWidth',LWidths,'NodeLabel',Names(1:N))
 
